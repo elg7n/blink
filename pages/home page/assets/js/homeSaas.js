3476a63d2ab5c3014038cec6ea116e5677f4fe48
Elgun <elgunam@code.edu.az>
2022-09-02 00:19:45 +0400

    testimonial section and carousel added

--- pages/home page/assets/js/homeSaas.js
+++ pages/home page/assets/js/homeSaas.js
@@ -9,6 +9,22 @@ window.onscroll = function () {
 };
 AOS.init();
 
-let counter=document.querySelector(".row .row");
-let counter_Up=new counterUp(counter);
+let counter = document.querySelector(".row .row");
+let counter_Up = new counterUp(counter);
 counter_Up.start();
+counter_Up.update();
+
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: false,
+    items: 2,
+    responsive: {
+        600: {
+            items: 1,
+        },
+        1000:{
+            items:2,
+        }
+    }
+});
