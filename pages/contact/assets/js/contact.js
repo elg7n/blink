092b57627422f17f1eb559fb4dbaa0f13ce66da9
Elgun <elgunam@code.edu.az>
2022-09-04 02:53:11 +0400

    dropdownJsForContact,updatePassage forMobile added

--- pages/contact/assets/js/contact.js
+++ pages/contact/assets/js/contact.js
@@ -16,3 +16,20 @@ let navbarToggler = document.querySelector(".mobile-menu-btn");
 navbarToggler.addEventListener('click', function () {
     navbarToggler.classList.toggle("active");
 });
+
+//  up/down arrow for hamburger dropdown
+let transforms=document.querySelectorAll("a.dd-menu");
+
+transforms.forEach(transform => {
+    transform.addEventListener("click",function(){
+        
+        if(!transform.classList.contains("collapsed")){
+
+            transform.style.setProperty('--transform','180deg')
+        }
+        if(transform.classList.contains("collapsed")){
+            transform.style.setProperty('--transform','0deg')
+        }
+        
+    })
+});
